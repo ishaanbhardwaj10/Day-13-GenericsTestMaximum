45e1780851342cf7968babf8bc38f87201b146b0
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-31 10:46:10 +0530

    called generic FindMax method for all data types

--- Program.cs
+++ Program.cs
@@ -4,20 +4,20 @@
     {
         public static void Main(string[] args)
         {
-
+                        
             //maximum integer
-            FindMax<int> obj = new FindMax<int>(22, 54, 62);
-            int result = obj.FindMaxShow();
+            FindMax<int> obj = new FindMax<int>(22, 54, 62, 87, 32);
+            int result = obj.Maxmethod();
             Console.WriteLine("Greatest number among three numbers is :  " + result);
 
             //maximum float
-            FindMax<float> objf = new FindMax<float>(22.6f, 22.8f, 22.6f);
-            float resultf = objf.FindMaxShow();
+            FindMax<float> objf = new FindMax<float>(22.6f, 22.8f, 22.6f, 65.6f, 65.9f);
+            float resultf = objf.Maxmethod();
             Console.WriteLine("Greatest number among three float number is :  " + resultf);
 
             //find maximum string
-            FindMax<string> objs = new FindMax<string>("Apple", "Peach", "Banana");
-            string results = objs.FindMaxShow();
+            FindMax<string> objs = new FindMax<string>("Apple", "Peach", "Banana", "Mango", "Stewberry");
+            string results = objs.Maxmethod();
             Console.WriteLine("Greatest string among three string is :  " + results);
 
         }
