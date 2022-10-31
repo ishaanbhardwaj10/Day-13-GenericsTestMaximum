5514c6d9db77fd7da71f0d32ad05b9f433053998
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-31 10:52:15 +0530

    called PrintMaxValue method on objects of all three data types

--- Program.cs
+++ Program.cs
@@ -3,22 +3,18 @@
     class Program
     {
         public static void Main(string[] args)
-        {
-                        
+        {     
             //maximum integer
             FindMax<int> obj = new FindMax<int>(22, 54, 62, 87, 32);
-            int result = obj.Maxmethod();
-            Console.WriteLine("Greatest number among three numbers is :  " + result);
+            obj.PrintMaxValue();
 
             //maximum float
-            FindMax<float> objf = new FindMax<float>(22.6f, 22.8f, 22.6f, 65.6f, 65.9f);
-            float resultf = objf.Maxmethod();
-            Console.WriteLine("Greatest number among three float number is :  " + resultf);
+            FindMax<float> objf = new FindMax<float>(22.6f, 122.8f, 22.6f, 65.6f, 65.9f);
+            objf.PrintMaxValue();
 
             //find maximum string
-            FindMax<string> objs = new FindMax<string>("Apple", "Peach", "Banana", "Mango", "Stewberry");
-            string results = objs.Maxmethod();
-            Console.WriteLine("Greatest string among three string is :  " + results);
+            FindMax<string> objs = new FindMax<string>("zApple", "Peach", "Banana", "Mango", "Stewberry");
+            objs.PrintMaxValue();
 
         }
     }
