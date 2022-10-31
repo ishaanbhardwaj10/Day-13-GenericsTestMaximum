108156b1f9acbc57e4ec442250c7fa8676556e83
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-31 09:46:23 +0530

    called the method for float values

--- Program.cs
+++ Program.cs
@@ -7,9 +7,15 @@
 
             FindMax findMax = new FindMax();
 
+            //maximum integer
             int result = findMax.FindMaxInteger(150, 100, 5);
-
             Console.WriteLine("Greatest number among three numbers is : " + result);
+
+            //maximum float
+            float result1 = findMax.FindMaxFloat(322.3f, 433.7f, 683.2f);
+            Console.WriteLine("\nGreatest number among three Float Numbers is : " + result1);
+
+
 
         }
     }
